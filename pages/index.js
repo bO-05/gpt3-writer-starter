0e36f9bacb2ba74e5b99c08716e5be6f5aaba6a7
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2023-01-02 17:12:03 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,98 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState(false);
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log('Calling OpenAI...');
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log('OpenAI replied...', output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+
+  const onUserChangedText = (event) => {
+    // console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>GPT-3 Writer | Buildspace</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Generate a business idea that leverages AI</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>
+              Write a short description of the business idea, we'll generate the
+              target market and potential value proposition.
+            </h2>
+            <h2>
+              (ex. "Personalized nutrition coaching service": A personalized
+              nutrition coaching service that uses AI to generate personalized
+              meal plans based on a user's nutritional needs and preferences.
+              Target market is health-conscious individuals looking to improve
+              their diet and reach specific health goals. Value proposition is
+              the use of AI to create highly customized and effective meal plans
+              at an affordable price.).
+            </h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <textarea
+            placeholder="start typing here"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={
+                isGenerating ? 'generate-button loading' : 'generate-button'
+              }
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Generate</p>
+                )}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="badge-container grow">
