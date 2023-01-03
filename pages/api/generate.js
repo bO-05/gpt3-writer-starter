6ec26fb4c48df2e8e20f9e4fd8c8db21c5c2ffd3
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2023-01-03 08:38:06 +0700

    prompt-chaining

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,26 +6,50 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix =
-// `
-// Write an in-depth target market and potential value proposition for the next business idea. The business idea is utilizing AI, and it's about:
-// `
-`Tulis target pasar yang mendalam dan proposisi nilai potensial untuk ide bisnis berikut. Ide Bisnis berikut ini memanfaatkan kecerdasan buatan (AI), dan ini tentang:`
+const basePromptPrefix = `
+Write an in-depth target market and potential value proposition for the next business idea. The business idea is utilizing AI, and the title is:
+
+Title:
+`;
 
 const generateAction = async (req, res) => {
-  // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.7,
-    max_tokens: 300,
+    temperature: 0.8,
+    max_tokens: 250,
   });
 
   const basePromptOutput = baseCompletion.data.choices.pop();
 
-  res.status(200).json({ output: basePromptOutput });
+  // I build Prompt #2.
+  const secondPrompt = `
+  Create a comprehensive, in-depth business plan for an AI-powered startup based on the target market, potential value proposition, and title of the business idea below.
+    
+  Title: ${req.body.userInput}
+
+  Target market and potential value proposition: ${basePromptOutput.text}
+
+  Business plan:
+  `;
+
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+    // I also increase max_tokens.
+    max_tokens: 1250,
+  });
+
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
 };
 
 export default generateAction;
