eb5a53457fb58eafdb5d6793011470d84e0aa6af
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2023-01-03 08:11:21 +0700

    change max_tokens

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -20,7 +20,7 @@ const generateAction = async (req, res) => {
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}`,
     temperature: 0.7,
-    max_tokens: 250,
+    max_tokens: 300,
   });
 
   const basePromptOutput = baseCompletion.data.choices.pop();
